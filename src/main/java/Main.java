6c85d9a30f910bba18953be70b0a48805a971d02
Kalani <kalani.gayathri91@gmail.com>
2022-05-25 11:56:07 -0700

    FibonacciSeries Sample Implementation

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import main.java.programs.basic.FibonacciSeries;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Fibonacci Series");
+        FibonacciSeries.FibonacciWithoutRecursion();
+    }
+}
---
+++ src/main/java/programs/basic/FibonacciSeries.java
@@ -0,0 +1,36 @@
+package main.java.programs.basic;
+
+/**
+ * Printing first 10 Fibonacci Series
+ * 0,1,1,2,3,5,8,13,21,34
+ */
+public class FibonacciSeries {
+    //Without using recursion
+    public static void FibonacciWithoutRecursion(){
+        int n1 = 0;
+        int n2 = 1;
+        int n3;
+        int count = 10;
+
+        System.out.print(n1 + ", " + n2);
+        for(int i = 2; i < count; i++){
+            n3 = n1 + n2;
+            System.out.print(", " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
+    public static void FibonacciWithRecursion(int n1, int n2, int count){
+        int n3;
+
+
+        System.out.print(n1 + ", " + n2);
+        for(int i = 2; i < count; i++){
+            n3 = n1 + n2;
+            System.out.print(", " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
